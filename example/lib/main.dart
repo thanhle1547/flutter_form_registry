f1c8f48a4e51fda5c47163eacb19acfc972777f4
thanhle1547 <thanhlm72@wru.vn>
2022-07-19 21:56:55 +0700

    update example
    (looking up the first Form's invalid field)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,45 +1,33 @@
+import 'package:example/my_text_field.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_registry/flutter_form_registry.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'custom_text_form_field.dart';
+
+/// A modification of
+/// [the example (looking up a Form's invalid field)](https://dartpad.dartlang.org/?id=120893372689ce66bbf89e5178848834)
+/// from issue [#67283](https://github.com/flutter/flutter/issues/67283).
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo Scroll To Invalid FormField',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Scroll to invalid field'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +36,112 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final GlobalKey<FormRegistryWidgetState> _registerdKey = GlobalKey();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String? integerTextFieldValidator(String? value) {
+    if (value == null) {
+      return 'Null!';
+    }
+    try {
+      int.parse(value);
+    } catch (error) {
+      return 'Not an integer!';
+    }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: FormRegistryWidget(
+          key: _registerdKey,
+          autoScrollToFirstInvalid: true,
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    for (int i = 0; i < 25; i++)
+                      CustomTextFormField(
+                        fieldName: "No. $i",
+                        initialValue: "$i",
+                        validator: integerTextFieldValidator,
+                      ),
+                    CustomTextFormField(
+                      fieldName: "No. 25",
+                      initialValue: 'thirty',
+                      validator: integerTextFieldValidator,
+                    ),
+                    //
+                    for (int i = 26; i < 30; i++)
+                      FormFieldRegisteredWidget(
+                        fieldName: "No. $i",
+                        validator: integerTextFieldValidator,
+                        buidler: (
+                          GlobalKey<FormFieldState<String>> formFieldKey,
+                          String? Function(String?) validator,
+                        ) {
+                          return MyTextField(
+                            initial: "$i",
+                            fieldKey: formFieldKey,
+                            validator: validator,
+                          );
+                        },
+                      ),
+                    FormFieldRegisteredWidget(
+                      fieldName: 'No. 30',
+                      validator: integerTextFieldValidator,
+                      buidler: (
+                        GlobalKey<FormFieldState<String>> formFieldKey,
+                        String? Function(String?) validator,
+                      ) {
+                        return MyTextField(
+                          initial: '30',
+                          fieldKey: formFieldKey,
+                          validator: validator,
+                        );
+                      },
+                    ),
+                    for (int i = 31; i < 60; i++)
+                      FormFieldRegisteredWidget(
+                        fieldName: 'No. $i',
+                        validator: integerTextFieldValidator,
+                        buidler: (
+                          GlobalKey<FormFieldState<String>> formFieldKey,
+                          String? Function(String?) validator,
+                        ) {
+                          return MyTextField(
+                            initial: "$i",
+                            fieldKey: formFieldKey,
+                            validator: validator,
+                          );
+                        },
+                      ),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          _formKey.currentState?.validate();
+
+          if (kDebugMode) {
+            print(_registerdKey.currentState!.firstError?.errorText);
+          }
+        },
+        tooltip: 'Scroll to invalid',
+        child: const Icon(Icons.search),
+      ),
     );
   }
 }
---
+++ example/lib/my_text_field.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+/// This is just a modification of
+/// [the example](https://dartpad.dartlang.org/?id=120893372689ce66bbf89e5178848834)
+/// from issue [#67283](https://github.com/flutter/flutter/issues/67283).
+class MyTextField extends StatelessWidget {
+  const MyTextField({
+    Key? key,
+    required this.initial,
+    required this.fieldKey,
+    this.validator,
+  }) : super(key: key);
+
+  final String initial;
+  final GlobalKey fieldKey;
+  final FormFieldValidator<String>? validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      key: fieldKey,
+      initialValue: initial,
+      autovalidateMode: AutovalidateMode.always,
+      validator: validator,
+    );
+  }
+}
